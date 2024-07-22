ad4d82b1082365e96a5848c574f6d5a1ac37cbbd
iNqir <151392133+Nqir@users.noreply.github.com>
2024-07-23 00:02:09 +0800

    chore: add tiktok gifts symbols

---
+++ src/lang/tiktokGifts.ts
@@ -0,0 +1,303 @@
+export const  TIKTOK_GIFT: { [key: string]: { icon: string, coins: number } } = {
+    "Gimme The Vote": { icon: "", coins: 1 },
+    "Community Fest": { icon: "", coins: 1 },
+    "Music Play": { icon: "", coins: 1 },
+    "GG": { icon: "", coins: 1 },
+    "Ice Cream Cone": { icon: "", coins: 1 },
+    "Rose": { icon: "", coins: 1 },
+    "TikTok": { icon: "", coins: 1 },
+    "Thumbs Up": { icon: "", coins: 1 },
+    "Heart": { icon: "", coins: 1 },
+    "Cake Slice": { icon: "", coins: 1 },
+    "Love you": { icon: "", coins: 1 },
+    "Football": { icon: "", coins: 1 },
+    "Rainbow": { icon: "", coins: 1 },
+    "Flame heart": { icon: "", coins: 1 },
+    "Birthday Cake": { icon: "", coins: 1 },
+    "Heart Puff": { icon: "", coins: 1 },
+    "Heart Me": { icon: "", coins: 1 },
+
+    "Team Bracelet": { icon: "", coins: 2 },
+
+    "Finger Heart": { icon: "", coins: 5 },
+    "Potato": { icon: "", coins: 5 },
+    "Smart": { icon: "", coins: 5 },
+    "Ladybug": { icon: "", coins: 5 },
+    "Tofu": { icon: "", coins: 5 },
+
+    "Applause": { icon: "", coins: 9 },
+    "Cheer You Up": { icon: "", coins: 9 },
+
+    "Friendship Necklace": { icon: "", coins: 10 },
+    "Rosa": { icon: "", coins: 10 },
+    "Tiny Diny": { icon: "", coins: 10 },
+    "ASMR Time": { icon: "", coins: 10 },
+    "Horseshoe": { icon: "", coins: 10 },
+    "Cherry Blossom Bunny": { icon: "", coins: 10 },
+
+    "Perfume": { icon: "", coins: 20 },
+
+    "Doughnut": { icon: "", coins: 30 },
+
+    "Sign language love": { icon: "", coins: 49 },
+
+    "Butterfly": { icon: "", coins: 88 },
+
+    "Family": { icon: "", coins: 90 },
+    "Sending strength": { icon: "", coins: 90 },
+    "Fist bump": { icon: "", coins: 90 },
+
+    "Paper Crane": { icon: "", coins: 99 },
+    "Little Crown": { icon: "", coins: 99 },
+    "Cap": { icon: "", coins: 99 },
+    "Hat and Mustache": { icon: "", coins: 99 },
+    "Honorable Person": { icon: "", coins: 99 },
+    "Breakthrough Star": { icon: "", coins: 99 },
+    "Kiss your Heart": { icon: "", coins: 99 },
+    "Hot Shot": { icon: "", coins: 99 },
+    "Guitar": { icon: "", coins: 99 },
+    "Like-Pop": { icon: "", coins: 99 },
+    "Birthday Crown": { icon: "", coins: 99 },
+    "Self care mask": { icon: "", coins: 99 },
+    "Community Crown": { icon: "", coins: 99 },
+    "Star": { icon: "", coins: 99 },
+
+    "Confetti": { icon: "", coins: 100 },
+    "Hand Hearts": { icon: "", coins: 100 },
+    "Hand Heart": { icon: "", coins: 100 },
+    "Bear love": { icon: "", coins: 100 },
+    "Marvelous Confetti": { icon: "", coins: 100 },
+
+    "Socks and Sandals": { icon: "", coins: 150 },
+
+    "Sunglasses": { icon: "", coins: 199 },
+    "Hearts": { icon: "", coins: 199 },
+    "Lock and Key": { icon: "", coins: 199 },
+    "Garland Headpiece": { icon: "", coins: 199 },
+    "Love You": { icon: "", coins: 199 },
+    "Cheer For You": { icon: "", coins: 199 },
+    "Jungle Hat": { icon: "", coins: 199 },
+    "Goalkeeper Save": { icon: "", coins: 199 },
+    "Sending positivity": { icon: "", coins: 199 },
+    "Meerkat": { icon: "", coins: 199 },
+    "Birthday Glasses": { icon: "", coins: 199 },
+    "Stinging Bee": { icon: "", coins: 199 },
+    "Massage for You": { icon: "", coins: 199 },
+
+    "Pinch Face": { icon: "", coins: 249 },
+
+    "Boxing Gloves": { icon: "", coins: 299 },
+    "Duck": { icon: "", coins: 299 },
+    "Corgi": { icon: "", coins: 299 },
+    "Dash": { icon: "", coins: 299 },
+    "Superpower": { icon: "", coins: 299 },
+    "Elephant trunk": { icon: "", coins: 299 },
+    "TikTok Crown": { icon: "", coins: 299 },
+    "Fruit Friends": { icon: "", coins: 299 },
+    "Play for you": { icon: "", coins: 299 },
+    "Fake smile": { icon: "", coins: 299 },
+    "Dancing Flower": { icon: "", coins: 299 },
+
+    "Singing Mic": { icon: "", coins: 399 },
+    "Cotton the Seal": { icon: "", coins: 399 },
+    "Forever Rosa": { icon: "", coins: 399 },
+    "Mic Check": { icon: "", coins: 399 },
+    "Magic Rhythm": { icon: "", coins: 399 },
+    "Sending luck": { icon: "", coins: 399 },
+    "Gaming Headset": { icon: "", coins: 399 },
+    
+    "Coral": { icon: "", coins: 499 },
+    "Hands Up": { icon: "", coins: 499 },
+
+    "Money Gun": { icon: "", coins: 500 },
+    "Prince": { icon: "", coins: 500 },
+    "Lion's Mane": { icon: "", coins: 500 },
+    "Window basket": { icon: "", coins: 500 },
+    "Dream Team": { icon: "", coins: 500 },
+    "Mic Drop": { icon: "", coins: 500 },
+    "Manifesting": { icon: "", coins: 500 },
+    "Sweet Memories": { icon: "", coins: 500 },
+    "You're Amazing": { icon: "", coins: 500 },
+    "VR Goggles": { icon: "", coins: 500 },
+    "Gem Gun": { icon: "", coins: 500 },
+
+    "Swan": { icon: "", coins: 699 },
+
+    "Claw machine": { icon: "", coins: 800 },
+
+    "Train": { icon: "", coins: 899 },
+    
+    "Hot Air Balloon": { icon: "", coins: 999 },
+    "Travel with You": { icon: "", coins: 999 },
+    "Lovely Music": { icon: "", coins: 999 },
+    "Lucky Airdrop Box": { icon: "", coins: 999 },
+    "Trending Figure": { icon: "", coins: 999 },
+
+    "Gold Mine": { icon: "", coins: 1000 },
+    "Disco ball": { icon: "", coins: 1000 },
+    "Mirror Bloom": { icon: "", coins: 1000 },
+    "Galaxy": { icon: "", coins: 1000 },
+    "Watermelon Love": { icon: "", coins: 1000 },
+    "Gerry the Giraffe": { icon: "", coins: 1000 },
+    "Drums": { icon: "", coins: 1000 },
+    "Glowing Jellyfish": { icon: "", coins: 1000 },
+    "Blooming Ribbons": { icon: "", coins: 1000 },
+    "Shiny air balloon": { icon: "", coins: 1000 },
+
+    "Fireworks": { icon: "", coins: 1088 },
+    "Diamond Tree": { icon: "", coins: 1088 },
+
+    "Moonlight flower": { icon: "", coins: 1400 },
+
+    "Garland": { icon: "", coins: 1500 },
+    "Champion": { icon: "", coins: 1500 },
+    "Breakthrough Celebration": { icon: "", coins: 1500 },
+    "Diamond Ring": { icon: "", coins: 1500 },
+    "Chasing the Dream": { icon: "", coins: 1500 },
+    "Pim Bear": { icon: "", coins: 1500 },
+    "Lover's Lock": { icon: "", coins: 1500 },
+    "Greeting Card": { icon: "", coins: 1500 },
+    "Future Encounter": { icon: "", coins: 1500 },
+    "Adventure Encounter": { icon: "", coins: 1500 },
+
+    "ASMR Starter Kit": { icon: "", coins: 1599 },
+
+    "Center Stage": { icon: "", coins: 1799 },
+    "Here We Go": { icon: "", coins: 1799 },
+
+    "Love Drop": { icon: "", coins: 1800 },
+
+    "Make-up Box": { icon: "", coins: 1999 },
+    "Star Adventures": { icon: "", coins: 1999 },
+    "Airship": { icon: "", coins: 1999 },
+    "Star of Red Carpet": { icon: "", coins: 1999 },
+    "Gift Box1": { icon: "", coins: 1999 },
+    "Cooper Flies Home": { icon: "", coins: 1999 },
+    "Wonder Strikes": { icon: "", coins: 1999 },
+    "Mystery Firework": { icon: "", coins: 1999 },
+
+    "Whale diving": { icon: "", coins: 2150 },
+
+    "Magic Stage": { icon: "", coins: 2599 },
+
+    "Concert": { icon: "", coins: 2888 },
+    "Motorcycle": { icon: "", coins: 2988 },
+
+    "Rhythmic Bear": { icon: "", coins: 2999 },
+
+    "Ferris Wheel": { icon: "", coins: 3000 },
+    "Meteor Shower": { icon: "", coins: 3000 },
+    "Car Drifting": { icon: "", coins: 3000 },
+    "Dancing Bears": { icon: "", coins: 3000 },
+    "Golden Party": { icon: "", coins: 3000 },
+
+    "Gift Box2": { icon: "", coins: 3999 },
+
+    "Flower Overflow": { icon: "", coins: 4000 },
+    "Gaming Keyboard": { icon: "", coins: 4000 },
+
+    "Breakthrough Superstar": { icon: "", coins: 4088 },
+
+    "Tractor": { icon: "", coins: 4099 },
+
+    "Private Jet": { icon: "", coins: 4888 },
+    "Leon the Kitten": { icon: "", coins: 4888 },
+    "Signature Jet": { icon: "", coins: 4888 },
+
+    "Knockout": { icon: "", coins: 4999 },
+
+    "Ellie the Elephant": { icon: "", coins: 5000 },
+    "Unicorn Fantasy": { icon: "", coins: 5000 },
+    "Bird Whisperer": { icon: "", coins: 5000 },
+    "Silver Sports Car": { icon: "", coins: 5000 },
+    "DJ Alien": { icon: "", coins: 5000 },
+    "Flying Jets": { icon: "", coins: 5000 },
+    "Motorcycle Together": { icon: "", coins: 5000 },
+    "Spirits Up": { icon: "", coins: 5000 },
+    "1st Anniversary": { icon: "", coins: 5000 },
+
+    "Wolf": { icon: "", coins: 5500 },
+
+    "With you": { icon: "", coins: 6000 },
+    "Future City": { icon: "", coins: 6000 },
+    "Sam in New City": { icon: "", coins: 6000 },
+
+    "Lili the Leopard": { icon: "", coins: 6599 },
+
+    "Celebration Time": { icon: "", coins: 6999 },
+    "Birthday Party": { icon: "", coins: 6999 },
+    "Happy Party": { icon: "", coins: 6999 },
+
+    "Sports Car": { icon: "", coins: 7000 },
+    "2nd Anniversary": { icon: "", coins: 7000 },
+
+    "Star Throne": { icon: "", coins: 7999 },
+
+    "Safari Park": { icon: "", coins: 8000 },
+
+    "Saxophone": { icon: "", coins: 9000 },
+
+    "Leon and Lili": { icon: "", coins: 9699 },
+
+    "Interstellar": { icon: "", coins: 10000 },
+    "Sunset Speedway": { icon: "", coins: 10000 },
+    "Octopus": { icon: "", coins: 10000 },
+
+    "Falcon": { icon: "", coins: 10999 },
+
+    "Race Car": { icon: "", coins: 12000 },
+    "Red Lightning": { icon: "", coins: 12000 },
+    "3rd Anniversary": { icon: "", coins: 12000 },
+    "Gimme The Mic": { icon: "", coins: 12000 },
+
+    "Fans Cheering": { icon: "", coins: 14999 },
+    "Golden Scepter": { icon: "", coins: 14999 },
+
+    "Mammoth": { icon: "", coins: 15000 },
+    "Speedster": { icon: "", coins: 15000 },
+    "Rosa Nebula": { icon: "", coins: 15000 },
+    "Berlin": { icon: "", coins: 15000 },
+    "Look! Meteor Shower": { icon: "", coins: 15000 },
+    "Sweetopia": { icon: "", coins: 15000 },
+    "Future Journey": { icon: "", coins: 15000 },
+    "Adventure Journey": { icon: "", coins: 15000 },
+    "Elephant Pals": { icon: "", coins: 15000 },
+
+    "Amusement Park": { icon: "", coins: 17000 },
+
+    "Fly Love": { icon: "", coins: 19999 },
+
+    "TikTok Shuttle": { icon: "", coins: 20000 },
+    "Castle Fantasy": { icon: "", coins: 20000 },
+    "Yacht": { icon: "", coins: 20000 },
+    "Spark": { icon: "", coins: 20000 },
+    "Premium Shuttle": { icon: "", coins: 20000 },
+
+    "Rose Carriage": { icon: "", coins: 25000 },
+
+    "Phoenix": { icon: "", coins: 25999 },
+    "Adam's Dream": { icon: "", coins: 25999 },
+    "Big Dino": { icon: "", coins: 25999 },
+    "Glory Arena": { icon: "", coins: 25999 },
+
+    "Dragon Flame": { icon: "", coins: 26999 },
+
+    "Lion": { icon: "", coins: 29999 },
+    "Knee Sliding": { icon: "", coins: 29999 },
+
+    "Sam the Whale": { icon: "", coins: 30000 },
+    "Gorilla": { icon: "", coins: 30000 },
+
+    "Leon and Lion": { icon: "", coins: 34000 },
+    "Zeus": { icon: "", coins: 34000 },
+
+    "TikTok Universe+": { icon: "", coins: 34999 },
+
+    "TikTok Stars": { icon: "", coins: 39999 },
+    "King Leonardo": { icon: "", coins: 39999 },
+    "Thunder Falcon": { icon: "", coins: 39999 },
+
+    "Fire Phoenix": { icon: "", coins: 41999 },
+    "Pegasus": { icon: "", coins: 42999 },
+    "TikTok Universe": { icon: "", coins: 44999 }
+}
