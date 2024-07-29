8212ee4efd12e2c12bbd47506699b57bc53b86ab
iNqir <151392133+Nqir@users.noreply.github.com>
2024-07-29 08:01:44 +0800

    Add `getRandomBlock` function

---
+++ src/utilities/blocks/randomBlock.ts
@@ -0,0 +1,10 @@
+import { BLOCKS } from "../../config";
+
+/**
+ * Returns a random block from the BLOCKS array.
+ * @returns {string} a random block from the BLOCKS array.
+ */
+export function getRandomBlock(): string {
+    const randomIndex = Math.floor(Math.random() * BLOCKS.length);
+    return BLOCKS[randomIndex];
+}
