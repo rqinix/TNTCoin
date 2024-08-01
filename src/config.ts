3eed9fe9ee3f5ab2c695f6545a08ff8253f576ae
iNqir <151392133+Nqir@users.noreply.github.com>
2024-08-02 06:23:48 +0800

    Add configuration constants for random blocks and sounds
    - Added `RANDOM_BLOCK_ITEM` constant for the item used to place random blocks.
    - Added `BLOCKS` array containing possible blocks for random placement.
    - Added `SOUNDS` array containing available sounds for playback.

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,32 @@
  * The item that will be used to open the TNT Coin GUI.
  */
 export const GUI_ITEM = 'tnt_coin:gui';
+
+/**
+ * The item that will be used to place random block.
+ */
+export const RANDOM_BLOCK_ITEM = 'minecraft:amethyst_block';
+
+/**
+ * List of possible blocks when placing random blocks.
+ */
+export const BLOCKS = [
+    'minecraft:stone',
+    'minecraft:dirt',
+    'minecraft:grass',
+    'minecraft:gravel',
+    'minecraft:sand',
+    'minecraft:diamond_block',
+    'minecraft:emerald_block',
+    'minecraft:gold_block',
+];
+
+/**
+ * List of sounds that you can play
+ */
+export const SOUNDS = [
+    {
+        name: 'Totem',
+        sound: 'random.totem'
+    }
+]
