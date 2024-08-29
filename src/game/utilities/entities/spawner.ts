f5411f78bfb56811dd32d7a354d47b9e6440a65f
iNqir <151392133+Nqir@users.noreply.github.com>
2024-08-29 21:04:33 +0800

    refactor: move `summonEntities()` to spawner utility file

---
+++ src/game/utilities/entities/spawner.ts
@@ -0,0 +1,53 @@
+import { Vector3 } from "@minecraft/server";
+import { TNTCoin } from "../../TNTCoin";
+import { clearBlocks } from "../blocks/clearing";
+import { batch } from "../batch";
+
+/**
+ * Summons entities in the game based on the provided options.
+ * @param {TNTCoin} game - The game instance to summon entities for.
+ * @param {string} entityName - The name of the entity to summon.
+ * @param {SummonOptions} options - The options for summoning entities.
+ */
+export function summonEntities(game: TNTCoin, entityName: string, options: SummonOptions): void {
+    const {
+        locationType = 'random', 
+        onTop = false,
+        amount = 1,
+        clearBlocksAfterSummon = false,
+        customLocations = [],
+        batchSize = 10, 
+        delayBetweenBatches = 0,
+        onSummon = () => {},
+    } = options;
+
+    let locations: Vector3[] = [];
+
+    if (customLocations.length > 0) {
+        locations = customLocations;
+    } else if (locationType === 'center') {
+        const { structureCenter, structureHeight } = game.structure;
+        const { x, y, z } = structureCenter;
+        const centerLocation = { x, y: onTop ? y + structureHeight + 5 : y + 2, z };
+        locations = Array(amount).fill(centerLocation);
+    } else if (locationType === 'random') {
+        locations = Array.from({ length: amount }, () => game.structure.randomLocation(2, onTop));
+    }
+
+    batch(
+        locations,
+        batchSize,
+        (location) => {
+            try {
+                game.player.dimension.spawnEntity(entityName, location);
+                onSummon();
+                if (clearBlocksAfterSummon) {
+                    clearBlocks(game.player.dimension, [location], 100);
+                }
+            } catch (error) {
+                game.feedback.error(`Failed to summon ${entityName} at location: ${JSON.stringify(location)}`, { sound: "item.shield.block" });
+            }
+        },
+        { delayInTicks: delayBetweenBatches }
+    );
+}
