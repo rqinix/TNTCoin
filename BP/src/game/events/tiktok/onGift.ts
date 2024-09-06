54d584b8ed04faa56299f8717f4ee3861cfcf4b7
iNqir <151392133+Nqir@users.noreply.github.com>
2024-09-06 21:48:19 +0800

    refactor(onGift): update onGift function to handle gift actions

--- BP/src/game/events/tiktok/onGift.ts
+++ BP/src/game/events/tiktok/onGift.ts
@@ -6,105 +6,74 @@ import { DEFAULT_GIFT, TIKTOK_GIFT } from "../../../lang/tiktokGifts";
  * @param {TNTCoin} game the TNT Coin game instance.
  * @param {string} message the message received from WebSocket Server.
  */
-export async function onGift(game: TNTCoin, message: string): Promise<void> { 
+export async function onGift(game: TNTCoin, message: string): Promise<void> {
     // Parse the message received from the WebSocket server (TNTCoin extension)
     const parsedMessage = JSON.parse(message);
     const { gifterUniqueId, gifterNickName, giftName, giftId, giftCount } = parsedMessage;
 
     let matchedGift = Object.values(TIKTOK_GIFT).find(gift => gift.id === giftId);
     if (!matchedGift) matchedGift = TIKTOK_GIFT[giftName];
+    const giftEmoji = (!matchedGift || matchedGift?.emoji === '') ? DEFAULT_GIFT : matchedGift.emoji;
 
-    const giftEmoji = matchedGift?.emoji || DEFAULT_GIFT;
+    const coloredNickName = `§e${gifterNickName}§r`;
+    const title = `${giftEmoji}\n§d${coloredNickName}§f`;
+    const subtitle = `§asent§f §g${giftName}§f §o§c x${giftCount}!`;
+    const chatMessage = `§aThank you for §c§o${giftCount} ${giftEmoji}§d${giftName}§r, §b${coloredNickName}§a!`;
+    const actions = game.giftActionManager.getActionsForGift(giftId?.toString()) || game.giftActionManager.getActionsForGift(giftName);
 
     const goalGiftName = game.giftGoal.getGiftName();
     const goalGiftId = game.giftGoal.getGiftId();
     const isGoalActive = game.giftGoal.isActive();
 
-    const coloredNickName = `§e${gifterNickName}§r`;
-    const chatMessage = `§aThank you for §c§o${giftCount} ${giftEmoji}§d${giftName}§r, §b${coloredNickName}§a!`;
-
-    // Define actions for each gift
-    const actions = {
-        summonTNT: (amount: number) => {
-            const title = `${giftEmoji}\n§b${giftName}§r\n§d${coloredNickName}§f`;
-            const subtitle = `§asent §cTNT §d§ox${amount}§f`;
-            const MESSAGE = `${giftEmoji} ${coloredNickName} sent §cTNT §d§ox${amount}§f!`
-
-            game.summonEntities(
-                {
-                    entityName: 'tnt_minecart',
-                    amount,
-                    locationType: 'random',
-                    onTop: true,
-                    batchSize: 5,
-                    delayBetweenBatches: 10,
-                    onSummon: () => game.player.playSound('kururin')
-                }
-            );
-            game.feedback.showFeedbackScreen({ title, subtitle });
-            game.player.sendMessage(MESSAGE);
-        },
-
-        summonLightning: (amount: number) => {
-            const title = `${giftEmoji}\n§d${coloredNickName}§f`;
-            const subtitle = `§asent §cLightning Bolts §d§ox${amount}§f!`;
-            const MESSAGE = `${giftEmoji} ${coloredNickName} sent §cLightning Bolts §d§ox${amount}§f!`;
-
-            game.summonLightningBolt(amount);
-            game.feedback.showFeedbackScreen({ title, subtitle });
-            game.player.sendMessage(MESSAGE);
-        },
-
-        clearBlocks: () => {
-            const title = `${giftEmoji}\n§d${coloredNickName}§f`;
-            const subtitle = `§acleared the blocks!`;
-            const MESSAGE = `${giftEmoji} ${coloredNickName} cleared the blocks!`;
-            const SOUND_ON_CLEAR = 'cat_laughing'; 
-
-            game.structure.clearFilledBlocks();
-
-            game.feedback.showFeedbackScreen({ title, subtitle });
-            game.player.playSound(SOUND_ON_CLEAR);
-            game.player.sendMessage(MESSAGE);
-        },
-
-        fillStructure: () => {
-            const title = `${giftEmoji}\n§d${coloredNickName}§f`;
-            const subtitle = `§afilled the structure!`;
-            const SOUND_ON_FILL = 'wait_wait_wait';
-
-            game.feedback.showFeedbackScreen({ title, subtitle });
-            game.player.playSound(SOUND_ON_FILL);
-            game.structure.fill();
-        },
-
-        // ... Add your own actions here
-    };
-
-    // TikTok may change the gift names, so we need to update this list and the gift names in the lang folder
-    // For example, TikTok changed the gift name 'Friendship Necklace' to 'BFF Necklace'
-    const giftActions: Array<{ id: string, name: string, action: () => Promise<void> | void }> = [
-        { id: '5655', name: 'Rose', action: () => actions.summonTNT(giftCount * 10) },
-        { id: '10749', name: 'Community Fest', action: () => actions.summonTNT(giftCount * 15) },
-        { id: '6671', name: 'Love You', action: () => actions.summonTNT(giftCount * 1) },
-        { id: '6064', name: 'GG', action: () => actions.summonLightning(giftCount * 3) },
-        { id: '8913', name: 'Rosa', action: actions.clearBlocks },
-        { id: '9947', name: 'BFF Necklace', action: actions.fillStructure },
-    ];
-
     if (isGoalActive && (goalGiftName === giftName || goalGiftId === giftId)) {
         game.giftGoal.addGifts(giftCount);
-        game.player.playSound('random.orb');
+    }
+
+    if (actions && actions.length > 0) {
+        actions.forEach((action: GiftAction) => {
+            if (action.actionType !== 'Summon' && action?.playSound) {
+                game.feedback.playSound(action.playSound);
+            }
+
+            switch (action.actionType) {
+                case 'Summon':
+                    game.feedback.showFeedbackScreen({
+                        title,
+                        subtitle: `${subtitle}§r\n§asent§f §d${action.summonOptions.entityName.toUpperCase()}§f §o§c x${action.summonOptions.amount}!`
+                    });
+                    game.summonEntities({
+                        entityName: action.summonOptions.entityName,
+                        amount: action.summonOptions.amount,
+                        locationType: action.summonOptions.locationType,
+                        onTop: action.summonOptions.onTop,
+                        batchSize: action.summonOptions.batchSize,
+                        batchDelay: action.summonOptions.batchDelay,
+                        onSummon: () => game.feedback.playSound(action.playSound),
+                    });
+                    break;
+                case 'Clear Blocks':
+                    game.feedback.showFeedbackScreen({
+                        title,
+                        subtitle: `${subtitle}§r\n§c!!!Cleared the Blocks!!!`
+                    });
+                    game.structure.clearFilledBlocks();
+                    break;
+                case 'Fill':
+                    game.feedback.showFeedbackScreen({
+                        title,
+                        subtitle: `${subtitle}§r\n§b!!!Filled the Structure!!!`
+                    });
+                    game.structure.fill();
+                    break;
+                case 'Play Sound':
+                    game.feedback.playSound(action.playSound);
+                    break;
+            }
+        });
+    } else {
+        game.feedback.showFeedbackScreen({ title, subtitle });
     }
 
     game.player.sendMessage(chatMessage);
-
-    const giftAction = giftActions.find(gift => gift.id === giftId || gift.name === giftName)?.action;
-    if (giftAction) {
-        await giftAction();
-    } else {
-        const title = `${giftEmoji}\n§d${coloredNickName}§f`;
-        const subtitle = `§asent§f §g${giftName}§f §o§c x${giftCount}!`;
-        game.feedback.showFeedbackScreen({ title, subtitle });
-    }
+    game.feedback.playSound('random.orb');
 }
