2e6674bf560e59d990b7eb17b1e8ac8ffba96578
rqinix <i.nqir.sov@gmail.com>
2025-05-18 22:47:48 +0800

    refactor(camera): introduce CameraUtils class and enhance rotateCamera360
    - Added a `clearTaskCamera` utility method
    - Enhanced error handling and camera state clearing within the rotation task.

---
+++ BP/src/utilities/camera/CameraUtils.ts
@@ -0,0 +1,71 @@
+import { Player, Vector3, EasingType } from "@minecraft/server";
+import { taskManager } from "../../core/Managers/TaskManager";
+import MathUtils from "../math/MathUtils";
+
+interface Camera360Options {
+    structurePosition: Vector3;
+    radius: number; 
+    height: number; 
+}
+
+export default class CameraUtils {
+    public static rotateCamera360(player: Player, camera360Options: Camera360Options, tickInterval: number): string {
+        player.camera.clear();
+
+        const taskId = `rotateCamera360:${player.name}`;
+        let angle = 0;
+
+        const updateCamera = () => {
+            if (!taskManager.has(taskId)) {
+                player.camera.clear();
+                return;
+            }
+
+            const radians = MathUtils.toRadians(angle);
+            const cameraX = camera360Options.structurePosition.x + camera360Options.radius * Math.cos(radians);
+            const cameraZ = camera360Options.structurePosition.z + camera360Options.radius * Math.sin(radians);
+            const cameraY = camera360Options.structurePosition.y + camera360Options.height;
+
+            const cameraCurrentPosition = { x: cameraX, y: cameraY, z: cameraZ };
+
+            const targetToLookAt: Vector3 = camera360Options.structurePosition;
+
+            const cameraOptions = {
+                location: cameraCurrentPosition,
+                facingLocation: targetToLookAt,
+                easeOptions: {
+                    easeTime: 0.5,
+                    easeType: EasingType.Linear
+                }
+            };
+
+            const speed = 10;
+            angle = (angle + speed) % 360;
+
+            if (taskManager.has(taskId)) {
+                try {
+                    player.camera.setCamera('minecraft:free', cameraOptions);
+                } catch (e) {
+                    console.error(`Error setting camera: ${e}`);
+                    taskManager.clearTask(taskId);
+                    player.camera.clear();
+                    return;
+                }
+                taskManager.addTimeout(taskId, updateCamera, tickInterval);
+            } else {
+                player.camera.clear();
+            }
+        };
+
+        taskManager.addTimeout(taskId, updateCamera, tickInterval);
+        return taskId;
+    }
+
+    public static clearTaskCamera(player: Player, taskId: string): void {
+        const taskIdPrefix = `${taskId}:${player.name}`;
+        if (taskManager.has(taskIdPrefix)) {
+            taskManager.clearTask(taskIdPrefix);
+        }
+        player.camera.clear();
+    }
+}
